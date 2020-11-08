41d5359977f0a00aca07e52641c555ef9be620c5
Justin Carter <justin@vidpresso.com>
2020-11-08 14:12:16 -0500

    image only

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import fstl from './fstl.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="content">
+          <img src={fstl} className="fstl" alt="four seasons total landscaping" />
+          <div className="source">John Minchillo/AP Photo</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
